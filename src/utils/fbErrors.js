b9d48c07f9abf51b845ae2e9e9bee19e16b417b1
JPS4L4 <juansalabet1@gmail.com>
2024-03-30 20:22:28 -0500

    Update R y L

--- src/utils/fbErrors.js
+++ src/utils/fbErrors.js
@@ -6,6 +6,15 @@ export const fbErrors = (code) => {
     case "auth/invalid-email":
       return "Email invalido";
 
+    case "auth/missing-email":
+      return "Email es requerido";
+
+    case "auth/weak-password":
+      return "Contraseña debil";
+
+    case "auth/invalid-credential":
+      return "Correo o Contraseña incorrecto";
+
     default:
       return `Error no registrado: ${code}`;
   }
