4e43c2b88d745e5e8325ea02e293739919e1f58a
JPS4L4 <juansalabet1@gmail.com>
2024-04-04 23:44:55 -0500

    se agrega validaciones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,14 @@ const Navbar = () => {
     }
   };
 
-  const btnBlue = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 mr-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  const btnBlue =
+    "text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2";
 
-  const btnRed = "text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 mr-2 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+  const btnRed =
+    "text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2";
 
-  const btnGreen = "text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 mr-2 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+  const btnGreen =
+    "text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2";
 
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
@@ -30,33 +33,21 @@ const Navbar = () => {
         <div className="flex md:order-2">
           {user ? (
             <>
-              <NavLink
-                to="/"
-                className={btnBlue}
-              >
+              <NavLink to="/" className={btnBlue}>
                 Inicio
               </NavLink>
 
-              <button
-                onClick={handleClickLogOut}
-                className={btnRed}
-              >
+              <button onClick={handleClickLogOut} className={btnRed}>
                 Log Out
               </button>
             </>
           ) : (
             <>
-              <NavLink
-                to="/login"
-                className={btnBlue}
-              >
+              <NavLink to="/login" className={btnBlue}>
                 Login
               </NavLink>
 
-              <NavLink
-                to="/register"
-                className={btnGreen}
-              >
+              <NavLink to="/register" className={btnGreen}>
                 Register
               </NavLink>
             </>
